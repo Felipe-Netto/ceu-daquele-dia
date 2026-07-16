012115da81b899c1e95754d3af258ad0a09a13e8
Felipe-Netto <felipeg.alvesnetto@gmail.com>
2026-07-15 21:41:53 -0300

    build

--- app/criar/page.tsx
+++ app/criar/page.tsx
@@ -3,5 +3,5 @@ import CriarForm from './_components/CriarForm'
 
 export default async function CriarPage() {
   const preco = await getPreco()
-  return <CriarForm preco={preco} precoFormatado={formatPreco(preco)} />
+  return <CriarForm preco={preco} precoFormatado={formatPreco(preco)}/>
 }
